655e3f44d8796ce98e18f734c505ea928715eb91
Captain1965 <alex1965-03@mail.ru>
2021-12-01 18:08:07 +0300

    Добавлен исправленный вариант программы подсчета покупок

--- counter_product.rb
+++ counter_product.rb
@@ -9,20 +9,26 @@
 #Вычислить и вывести на экран итоговую сумму всех покупок в "корзине".
 product_h ={}
 name_product_h ={}
+i = 1
 loop do 
 puts 'Введите название товара , закончите ввод введя end'
 name_product = STDIN.gets.chomp
 break if name_product == "end"
 puts 'Введите цену товара'
-price_product = STDIN.gets.chomp.to_f
+product_price = STDIN.gets.chomp.to_f
 puts 'Введите количество купленного товарa '
-quantity_product = STDIN.gets.chomp.to_f
-product_h[:price] = price_product
-product_h[:quantity] = quantity_product  
-puts product_h    
-#hash.merge!(item2: 2)
+product_quantity = STDIN.gets.chomp.to_f
+#создаем имя вложенного хэша пплюс номер шага в цикле product_h = product_h.to_s + i.to_s
+#вложенный хэш  для каждого наименования товара должен быть новый product_h = {}
+#записываем в него цену и количество приобретаемого товара 
+# product_h[:price] = product_price product_h[:quantity] = product_quantity  
+product_h = product_h.to_s + i.to_s
+product_h = {}
+product_h[:price] = product_price
+product_h[:quantity] = product_quantity 
+#в хэш с ключом по имени товара записываем хэш с количеством и ценой товара
 name_product_h[name_product] = product_h
-name_product_h.merge(a:product_h)
+i +=1
 end
 puts name_product_h
 #определим количесво пар ключ-значение в хэше с наименованием покупок
@@ -33,4 +39,3 @@ n.times do
 	sum += name_product_h[keys_h][:price]*name_product_h[keys_h][:quantity]
 end	
 puts " Вы купили следующие товары #{name_product_h.keys} на сумму #{sum} денежных единиц"
-
